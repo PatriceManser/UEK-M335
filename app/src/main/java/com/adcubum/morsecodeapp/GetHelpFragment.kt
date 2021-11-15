073e517bbee1d8df2d81b5208ee07dda97d5260f
PatriceManser <manserpatrice@gmail.com>
2021-11-15 15:36:05 +0100

    Added two different fragments to the tabs

---
+++ app/src/main/java/com/adcubum/morsecodeapp/GetHelpFragment.kt
@@ -0,0 +1,16 @@
+package com.adcubum.morsecodeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GetHelpFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_get_help, container, false)
+    }
+}
--- app/src/main/java/com/adcubum/morsecodeapp/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/adcubum/morsecodeapp/ui/main/SectionsPagerAdapter.kt
@@ -4,24 +4,24 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.adcubum.morsecodeapp.GetHelpFragment
 import com.adcubum.morsecodeapp.R
+import com.adcubum.morsecodeapp.SeeRequestsFragment
 
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
+    R.string.tab_text_1,
+    R.string.tab_text_2
 )
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
-    : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        return if (position != 1) GetHelpFragment() else SeeRequestsFragment()
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
